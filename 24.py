77e6b482fb377d633d18619bf0e695e3e4acf455
MK1HUNNA786 <missbahulquran786@gmail.com>
2025-02-22 15:40:22 +0000

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,2 +1,40 @@
 print("π")
+# Python works in a numbers as first priority way in the QWERTY keyboard format. 
+π = 3.142
+M = 69
+r = 0
+M = 786
+i = 786
+s = 2343
+s = 53464
+b = 6543
+a = 45
+h = 2039
+U = 1221
+l = 43
+H = 45
+a = 1
+q = 0o2342
+K = 42323
+h = 223
+a = 1223
+n = 53
+N = 2993
+i = 32478
+a = 34238
+z = 34223
+i = 43573
+print(M > i)
+print(M < i)
+print(M, i)
+
+AE16 = 22202000
+print(AE16)
+____________________ = 20 
+print(____________________)
+
+Literal = ____________________
+print(20)
+
+
 
